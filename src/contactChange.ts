e19110db8df88f9756cff37d7a6b4c4f3aaf5c5d
httq9302-svg <httq9302@gmail.com>
2026-07-24 22:23:05 +0900

    담당자 변경 양식 슬래시 제거

--- src/contactChange.ts
+++ src/contactChange.ts
@@ -22,7 +22,7 @@ export const EMPTY_CONTACT_CHANGE_FORM: ContactChangeFormState = {
 
 export function buildContactChangeText(form: ContactChangeFormState, author: string): string {
   return [
-    "/변경후담당자 명함 이미지있으면 첨부",
+    "변경후담당자 명함 이미지있으면 첨부",
     `업체명(본사/지점 등) : ${form.company}`,
     `퍼스트전산직원 : ${author}`,
     `수도권지역 : ${form.region}`,
